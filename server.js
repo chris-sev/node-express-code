6a9c646bf3f8ff84105cee037820cf07e773c046
Chris Sevilleja <chris@scotch.io>
2014-08-27 14:52:47 -0700

    almost done

---
+++ server.js
@@ -0,0 +1,34 @@
+// CONFIGURATION ====================
+// ==================================
+// load the things we need
+var express = require('express');
+var app     = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// set the path for all public resources (css/js/images)
+app.use(express.static(__dirname + '/public'));
+
+// ROUTES ===========================
+// ==================================
+// use res.render to load up an ejs view file
+// index page 
+app.get('/', function(req, res) {
+	res.render('./pages/home');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+// contact page 
+app.get('/about', function(req, res) {
+	res.render('pages/contact');
+});
+
+// START THE SERVER ================
+// =================================
+app.listen(8080);
+console.log('8080 is the magic port');
